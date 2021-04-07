fbc33c929b72fc434ffadbdc64675f687342f9c3
RBiyovskiy <82090471+RBiyovskiy@users.noreply.github.com>
2021-04-07 13:00:24 +0200

    Create Program.cs

---
+++ TestConsolAppDotNetCore/TestConsolAppDotNetCore/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using TestConsolAppDotNetCore.Utils;
+
+namespace TestConsolAppDotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port;
+
+            try
+            {
+                if (args.Length == 0)
+                    throw new Exception("Socket is not detrmined!");
+
+                if (!int.TryParse(args[0], out port))
+                    throw new Exception("Socket is bad!");
+
+                var ipAddr = IpAddressUtilities.GetLocalIPAddress();
+
+                Thread server = new Thread(delegate ()
+                {
+                    SocketServer myserver = new SocketServer(ipAddr, port);
+                });
+                server.Start();
+
+                Console.WriteLine("Server Started!");
+
+                while (true)
+                {
+                    string s = Console.ReadLine();
+                    if (s.Trim().ToLower() == "exit")
+                    {
+                        Environment.Exit(-1);
+                    }
+                    System.Threading.Thread.Sleep(100);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: {0}", ex.Message);
+            }
+        }
+    }
+}
